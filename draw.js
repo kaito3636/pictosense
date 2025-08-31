b34c6c6a3bb218641e98acbead878f7052e3b8fd
kaito3636 <iphone.8170_1002@icloud.com>
2025-08-31 19:31:40 +0900

    Update draw.js

--- draw.js
+++ draw.js
@@ -1,49 +1,32 @@
-window.startUpload = function(){
-  let i=document.createElement('input');
-  i.type='file'; 
-  i.accept='image/*';
-  i.onchange=e=>{
-    let f=e.target.files[0]; if(!f)return;
-    let r=new FileReader();
-    r.onload=ev=>{
-      // 画像表示用の要素
-      let img=document.createElement('img');
-      img.src=ev.target.result;
-      img.style.maxWidth="90%";
-      img.style.maxHeight="80vh";
-      img.style.display="block";
-      img.style.margin="20px auto";
-      document.body.appendChild(img);
+(function(){
+  // Cropper.js の CSS 読み込み
+  let css=document.createElement('link');
+  css.rel='stylesheet';
+  css.href='https://unpkg.com/cropperjs/dist/cropper.min.css';
+  document.head.appendChild(css);
 
-      // 決定ボタン
-      let btn=document.createElement('button');
-      btn.innerText="決定";
-      btn.style.display="block";
-      btn.style.margin="10px auto";
-      document.body.appendChild(btn);
+  // Cropper.js の JS 読み込み
+  let js=document.createElement('script');
+  js.src='https://unpkg.com/cropperjs/dist/cropper.min.js';
+  js.onload=()=>{
 
-      // Cropper有効化
-      let cropper=new Cropper(img,{
-        aspectRatio:NaN,
-        viewMode:1,
-        autoCropArea:1
-      });
+    // 画像選択ダイアログ
+    let i=document.createElement('input');
+    i.type='file';
+    i.accept='image/*';
+    i.onchange=e=>{
+      let f=e.target.files[0]; if(!f)return;
+      let r=new FileReader();
+      r.onload=ev=>{
+        // 画像表示
+        let img=document.createElement('img');
+        img.src=ev.target.result;
+        img.style.maxWidth="90%";
+        img.style.maxHeight="70vh";
+        img.style.display="block";
+        img.style.margin="20px auto";
+        img.id="crop-target";
+        document.body.appendChild(img);
 
-      btn.onclick=()=>{
-        let cvs=document.querySelector('canvas');
-        if(!cvs){alert("キャンバスがありません");return;}
-        let ctx=cvs.getContext('2d');
-        ctx.clearRect(0,0,cvs.width,cvs.height);
-
-        let cropped=cropper.getCroppedCanvas({width:cvs.width,height:cvs.height});
-        ctx.drawImage(cropped,0,0,cvs.width,cvs.height);
-
-        // UIを消す
-        img.remove();
-        btn.remove();
-      };
-    };
-    r.readAsDataURL(f);
-  };
-  i.click();
-};
+        // 決定ボタン
+        let
