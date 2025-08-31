5e75cc8dc3c3683051e9de6c092ed1e4eaa6ff90
kaito3636 <iphone.8170_1002@icloud.com>
2025-08-31 18:27:47 +0900

    Update draw.js

--- draw.js
+++ draw.js
@@ -1,24 +1,26 @@
 (function(){
-    var i=document.createElement('input');
-    i.type='file'; i.accept='image/*';
-    i.style.position='fixed'; i.style.left='-9999px';
-    document.body.appendChild(i);
+  var i = document.createElement('input');
+  i.type = 'file'; 
+  i.accept = 'image/*';
+  i.style.position = 'fixed'; 
+  i.style.left = '-9999px';
+  document.body.appendChild(i);
 
-    i.onchange=function(ev){
-        var f=ev.target.files[0];
-        if(!f){document.body.removeChild(i);return;}
-        var r=new FileReader();
-        r.onload=function(ev){
-            var img=new Image();
-            img.onload=function(){
-                var cvs=document.querySelector('#cvs');
-                if(!cvs){alert('キャンバスがありません');return;}
-                var ctx=cvs.getContext('2d');
-                ctx.drawImage(img,0,0,cvs.width,cvs.height);
-            };
-            img.src=ev.target.result;
-        };
-        r.readAsDataURL(f);
+  i.onchange = function(ev){
+    var f = ev.target.files[0];
+    if(!f){ document.body.removeChild(i); return; }
+    var r = new FileReader();
+    r.onload = function(ev){
+      var img = new Image();
+      img.onload = function(){
+        var cvs = document.querySelector('#cvs');
+        if(!cvs){ alert('キャンバスがありません'); document.body.removeChild(i); return; }
+        var ctx = cvs.getContext('2d');
+        ctx.drawImage(img, 0, 0, cvs.width, cvs.height);
+      };
+      img.src = ev.target.result;
     };
-    i.click();
+    r.readAsDataURL(f);
+  };
+  i.click();
 })();
